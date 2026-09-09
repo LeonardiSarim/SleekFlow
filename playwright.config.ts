459fff1ebb05862a5288dce3525d0aef203dee0d
LeonardiSarim <sarim.leonardi@gmail.com>
2026-09-09 12:54:24 +0800

    adding tracing ability when test failed in config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,13 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+
+    /* Capture screenshot automatically on test failure */
+    screenshot: 'only-on-failure',
+
+    /* Record video automatically on test failure */
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
